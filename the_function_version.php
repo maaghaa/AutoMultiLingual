f7ffa11bc8a5efb7e3fd5e48eed56015322558e2
Amin Ghaseminia <maghvf@gmail.com>
2023-07-10 15:21:17 +0330

    Added comments

--- the_function_version.php
+++ the_function_version.php
@@ -17,25 +17,32 @@ function Lang(string $text){
 
     //trim and lower the text so that non-important changes in the string don't cause inserting duplicated rows
     $text=strtolower(trim($text));
-    
+
+    //create the hash for better search
     $hash=md5($text);
 
+    //check whether the has already exists in the database
     $rt=mysqli_query($con,"SELECT $lang FROM translation WHERE hash='$hash'");
 
     if (mysqli_num_rows($rt)>0){
-        $rt=mysqli_fetch_array(0);
+        $rt=mysqli_fetch_array($rt)[0];
     }else{
         mysqli_query($con,"INSERT INTO translation (hash, timestamp, en) VALUES ('$hash', time(), '$text')");
     }
     
+    //if the translation is not available (yet) just return thr original string.
     if (empty($rt)) $rt=$text;
 
+    //get extra args passes to the function to replace (WW)s with.
     $args=func_get_args();
 
+
+    //Do the replacement
     for($a=1;$a<count($args);$a++){
         $rt=preg_replace('/WW/',trim($args[$a]),$rt,1);
     }
 
+    //Return the final string
     return $rt;
 }
 
